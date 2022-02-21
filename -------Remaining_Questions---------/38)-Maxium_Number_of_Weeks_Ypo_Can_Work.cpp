197cc253bae7b2c0c0ad8b7a33c6e8011a5e6463
devraj2018 <shekhawatdevraj123@gmail.com>
2022-02-21 21:06:14 +0530

    Update

---
+++ -------Remaining_Questions---------/38)-Maxium_Number_of_Weeks_Ypo_Can_Work.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long numberOfWeeks(vector<int>& milestones) {
+        
+        int max_no= *max_element(milestones.begin(),milestones.end());
+        
+        long long total=0;
+        for(auto x:milestones)
+         {
+            total+= x;
+         }
+        
+        long long remaining= total- max_no;
+        if(remaining>max_no || remaining== max_no ||  remaining+1 == max_no) return total;
+        
+        return 2*remaining+1;
+        
+        
+    }
+};
