39a5f07f600671be490a98456e9f6d3b5d071f55
devraj2018 <shekhawatdevraj123@gmail.com>
2022-03-10 09:10:35 +0530

    Update

---
+++ Amazon_Most_Frequently_Asked/72)-Minimum_Jumps_Required_To_sit_all_Together.cpp
@@ -0,0 +1,28 @@
+int Solution::seats(string A) {
+    
+    int n=A.length();
+    int mod=1e7+3;
+
+    vector<int>pos;    //  To store position of 'X'
+    int count=0;
+
+    for(int i=0;i<n;i++)
+    {
+        if(A[i]=='x')
+        {   pos.push_back(i-count);   //  (i-count) bcz it will give from mid how many position to be shifted
+            count++;
+        }
+    }
+
+    if(count==0 || count==n) return 0;
+    int median_index= (count)/2;
+    int ans=0;
+    for(int i=0;i<count;i++)
+    {
+        ans = (ans%mod + abs(pos[i]-pos[median_index])%mod)%mod;
+    }
+    return ans;
+     
+
+
+}
