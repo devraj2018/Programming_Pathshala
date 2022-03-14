3fd9f0802a247dfff097167c544d323347309c53
devraj2018 <shekhawatdevraj123@gmail.com>
2022-03-14 23:19:24 +0530

    Update

---
+++ Stacks/22)-Remove_All_Adjacent_Duplicates_having_Count_equal_K.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        
+        stack<pair<char,int>>stk;
+        int n=s.length();
+        for(int i=0;i<n;i++)
+        {
+             if(stk.empty()) stk.push({s[i],1});
+             else{
+                 
+                 if(s[i]==stk.top().first){
+                     stk.push({s[i],stk.top().second+1});
+                 }
+                 else stk.push({s[i],1});
+                 
+                 if(stk.top().second==k)
+                 { 
+                     int x=k;
+                     while(x--) stk.pop();
+                 }
+          }
+        }
+        string ans="";
+        while(!stk.empty())
+        {
+            ans+= stk.top().first;
+            stk.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
