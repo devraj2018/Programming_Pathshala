d0893e4edb35398e1c282d843716d8a1059d6f4d
devraj2018 <shekhawatdevraj123@gmail.com>
2022-04-10 14:02:04 +0530

    Update

--- Amazon_Most_Frequently_Asked/58)-Word_Ladder.cpp
+++ Amazon_Most_Frequently_Asked/58)-Word_Ladder.cpp
@@ -40,3 +40,55 @@ public:
         return 0;
      }
 };
+
+
+-------------------- Same Just Without Taking pair ----------------------------
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        unordered_map<string,int>validWord;  // To check word is present or not
+        for(auto s:wordList) validWord[s]++;
+        
+        unordered_set<string>visited;      
+        
+        if(validWord.find(endWord)==validWord.end()) return 0;  // If end Word is not present
+        if(beginWord==endWord) return 0;    // Both are same word
+        
+        queue<string>q;
+        q.push(beginWord);
+        visited.insert(beginWord);
+        int ans=0;
+        
+        while(!q.empty())
+        {
+            int size=q.size();
+            ans++;
+            for(int i=0;i<size;i++)
+            {
+                string curr= q.front();
+                q.pop();
+                
+                if(curr==endWord) return ans;      // If we found the word
+                
+                for(int j=0;j<curr.length();j++)
+                {    char x=curr[j];              // Store to backtrack
+                    
+                     for(int k=0;k<26;k++)
+                            {
+                               curr[j]='a'+k;     // Change jth index charcater with a to z
+                               if(validWord.find(curr)!=validWord.end() && visited.find(curr)==visited.end())
+                                 {
+                                    q.push(curr);
+                                    visited.insert(curr);
+                                 }
+                            }
+                     curr[j]=x;
+                }
+             }
+         }
+        return 0;
+        
+    }
+};
