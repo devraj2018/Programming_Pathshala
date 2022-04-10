bcb232c18d7998398a5d41505bf1475dd7c6c373
devraj2018 <shekhawatdevraj123@gmail.com>
2022-04-10 21:43:07 +0530

    Update

--- Graph/2)-Minimum_Steps_By_Knight.cpp
+++ Graph/2)-Minimum_Steps_By_Knight.cpp
@@ -1,45 +1,38 @@
-struct Info{
-    int x,y,dis;
-    Info(int _x,int _y,int _d)
+class Solution 
     {
-        x= _x;
-        y= _y;
-        dis= _d;
-    }
-};
-class Solution {
-public:
+    public:
+    //Function to find out minimum steps Knight needs to reach target position.
     int dx[8]={-1,-1,1,1,-2,-2,2,2};
     int dy[8]={2,-2,2,-2,1,-1,1,-1};
-	int minStepToReachTarget(vector<int>&start, vector<int>&target, int N){
-	     vector<vector<int>> vis(N+1,vector<int>(N+1,0));   
-	     queue<Info>q;
-	     q.push(Info(start[0],start[1],0));
-	     vis[start[0]][start[1]]=1;
+	int minStepToReachTarget(vector<int>&KnightPos,vector<int>&TargetPos,int N)
+	{
+	     queue<vector<int>>q;
+	     q.push({KnightPos[0],KnightPos[1],0});
+	     
+	     bool visited[N+1][N+1]={false};
+	     visited[KnightPos[0]][KnightPos[1]]=true;
+	     
 	     while(!q.empty())
-	     { int x=q.front().x;
-	        int y=q.front().y;
-	          int dis=q.front().dis;
-	          q.pop();
-	          
-	          if(x==target[0] && y==target[1])
-	            return dis;
-	        
-	          for(int i=0;i<8;i++)
-	          {  int new_x=x+dx[i];
-	             int new_y=y+dy[i];
-	             
-	             if(new_x>=1 && new_y>=1 && new_x<=N && new_y <=N &&vis[new_x][new_y]==0)
-	             {
-	                q.push(Info(new_x,new_y,dis+1));
-	                vis[new_x][new_y]=1;
-	            }
-	              
-	          }
+	     {
+	         vector<int> curr=q.front();
+	         q.pop();
 	         
-	     }
-	     return 0;
-	     
-	     
+	         int x=curr[0]; int y=curr[1]; int distance=curr[2];
+	         
+	         if(x==TargetPos[0] && y==TargetPos[1]) return distance;
+	         
+	         for(int i=0;i<8;i++)
+	         {
+	             int newx=x+dx[i];
+	             int newy=y+dy[i];
+	             
+	             if(newx>=1 && newy>=1 && newx<=N && newy<=N && visited[newx][newy]==false)
+	             {   q.push({newx,newy,distance+1});
+	                 visited[newx][newy]=true;
+	             }
+	         }
+	    }
+	   return -1;
+	 	     
 	}
 };
