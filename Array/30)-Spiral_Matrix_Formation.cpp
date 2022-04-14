9351aad70a56e52aabbff3b52e32b1aaff492edf
devraj2018 <shekhawatdevraj123@gmail.com>
2022-04-14 23:09:38 +0530

    Update

---
+++ Array/30)-Spiral_Matrix_Formation.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        
+        int up=0,left=0,right=n-1,down=n-1;
+        int counter=0;
+        vector<vector<int>>res(n,vector<int>(n));
+        while(up<=down && left<=right)
+        {
+            if(left>right || up>down) break;
+            for(int j=left;j<=right;j++) res[up][j]=++counter;
+            up++;
+            
+            if(left>right || up>down) break;
+            for(int i=up;i<=down;i++) res[i][right]=++counter;
+            right--;
+            
+            if(left>right || up>down) break;
+            for(int j=right;j>=left;j--)  res[down][j]=++counter;
+            down--;
+            
+            if(left>right || up>down) break;
+            for(int i=down;i>=up;i--) res[i][left]=++counter;
+            left++;
+          
+        }
+        return res;
+        
+        
+    }
+};
