22ada78f2365e53e1cdfb3f8592a0ba2cc26ed2b
devraj2018 <shekhawatdevraj123@gmail.com>
2022-06-08 01:51:08 +0530

    Updated

---
+++ --Striver_SDE_SHEET/46)-Check_Indentical_Tree.cpp
@@ -0,0 +1,8 @@
+
+bool identicalTrees(BinaryTreeNode<int>* root1, BinaryTreeNode<int>* root2) {
+   if(root1==NULL && root2==NULL) return true;
+        if(root1==NULL || root2==NULL) return false;
+     if(root1->data!=root2->data) return false;
+    
+    return identicalTrees(root1->left,root2->left) && identicalTrees(root1->right,root2->right) ;	 
+}
